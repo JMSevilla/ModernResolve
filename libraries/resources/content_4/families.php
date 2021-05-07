9259513e2818e8cde7bda7562fc72d88cf44f192
emman127 <cityjoker127@gmail.com>
2021-05-07 17:32:04 +0800

    First Commit

---
+++ libraries/resources/content_4/families.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <div class="container">
+                <h3 class="text-muted text-primary">FOR FAMILIES</h3>
+                <p class="fs-4 fw-bold">Updates that parents want</a>
+                <p class="fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias nisi expedita voluptatum, 
+                    natus ducimus mollitia neque libero, vero commodi suscipit omnis odio laudantium distinctio quisquam temporibus necessitatibus itaque officia.</p>
+                <a href="http://" class="text-warning fs-5" id="showme_hover">Tell me more</a>           
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="https://a.edim.co/images_v2/landing/rebrand/translated_ltr/module_4_img_ltr_trans.webp" alt="" width="100%" height="100%">
+        </div>
+    </div>
+</div>
--- views/index.views.php
+++ views/index.views.php
@@ -1,12 +1,9 @@
 
-<div class="layer-1">
-  <div class="container">
-    <div class="card">
-      <div class="card-body">
-        <h3>Test</h3>
-        <?php include("libraries/resources/forms.php"); ?>
-      </div>
-    </div>
-  </div>
-</div>
+  <?php include('libraries/resources/content.views.php'); ?>
 
+    
+
+   
+
+
+    
