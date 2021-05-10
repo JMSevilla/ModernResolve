3157e63cd27c687aba15168531c606c9a14ee2ce
torrestechnology <miggysvll@gmail.com>
2021-05-10 15:29:59 -0700

    fix email and routing

--- app/Http/Controller/postController.php
+++ app/Http/Controller/postController.php
@@ -1,12 +1,19 @@
 <?php
+
 class postController extends DBIntegration {
   public function postControl($table,$data){
     if($this->ControllerPrepare(iController_Insertion($table))){
       $this->bind(":fname", $data['fname']);
       if($this->ControllerExecutable()){
+        sendEmail();
         echo $this->SuccessJSONResponse();
       }
     }
   }
 }
 
+
+function sendEmail(){
+  $mail = new PHPMailer\PHPMailer\PHPMailer();
+   $this->emailsender($mail);    
+}
