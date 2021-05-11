38c1bc492f2917aaa7d7c92ebee52b3c6bea22ee
emman127 <cityjoker127@gmail.com>
2021-05-12 00:34:33 +0800

    test

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,12 +1,32 @@
 <?php
-spl_autoload_register('provide_root');
+// spl_autoload_register('provide_root');
 if(isset($_POST['TaskTrigger']) == 1){
     // echo json_encode(array($));
-   $callback = new Post();
-   $callback->model_signup($_POST['table']);
+    $data = [
+        'classcode' => $_POST['classcode'],
+        'fname' => $_POST['fname'],
+        'lname' => $_POST['lname'],
+        'bdate' => $_POST['bdate'],
+        'age' => $_POST['age'],
+        'contact' => $_POST['contact'],
+        'address' => $_POST['address'],
+        'province' => $_POST['province'],
+        'city' => $_POST['city'],
+        'street' => $_POST['street'],
+        'zipcode' => $_POST['zipcode'],
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+        'code' => $_POST['code'],
+        'sex' => $_POST['sex'],
+        'course' => $_POST['course'],
+      ];
+
+    //   $controller = new postController();
+    //   $controller->controller_signup($data);
 }
-function provide_root(){
-    include_once "../Route/webapi.php";
-    $calls = new web_api();
-    $calls::middleware("db.php", "Post.php", "postController.php", "queries.php", "PHPMailer.php", "SMTP.php", "Exception.php");
-}
+
+// function provide_root(){
+//     include_once "../Route/webapi.php";
+//     $calls = new web_api();
+//     $calls::middleware("db.php", "Post.php", "postController.php", "queries.php", "PHPMailer.php", "SMTP.php", "Exception.php");
+// }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,11 +22,8 @@ class Post extends DBIntegration {
     }
   }
 
-  public function model_signup($table){
-    if($this->model_prepare(models_check($table))) {
-      $this->bind(':mytable', $table);
-      if($this->ModelsExecution()) {
-        if($this->ModelsCount()) {
+  // Emman
+  public function model_signup(){
           $data = [
             'classcode' => $_POST['classcode'],
             'fname' => $_POST['fname'],
@@ -47,34 +44,7 @@ class Post extends DBIntegration {
           ];
 
           $controller = new postController();
-          $controller->controller_signup($table, $data);
-        }
-        else {
-          $sql = iModels_table($table);
-          $this->ModelsCreated($sql);
-          $data = [
-            'classcode' => $_POST['classcode'],
-            'fname' => $_POST['fname'],
-            'lname' => $_POST['lname'],
-            'bdate' => $_POST['bdate'],
-            'age' => $_POST['age'],
-            'contact' => $_POST['contact'],
-            'address' => $_POST['address'],
-            'province' => $_POST['province'],
-            'city' => $_POST['city'],
-            'street' => $_POST['street'],
-            'zipcode' => $_POST['zipcode'],
-            'email' => $_POST['email'],
-            'password' => $_POST['password'],
-            'code' => $_POST['code'],
-            'sex' => $_POST['sex'],
-            'course' => $_POST['course'],
-          ];
-
-          $controller = new postController();
-          $controller->controller_signup($table, $data);
-        }
-      }
+          $controller->controller_signup($data);
     }
   }
 }
