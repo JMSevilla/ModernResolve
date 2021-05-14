14b87f2bbd6a33ec0d114826329c92940da2a524
ellamarie_ <cyniella16@gmail.com>
2021-05-14 23:54:28 +0800

    login page revision 1

--- forgotpass.php
+++ forgotpass.php
@@ -1,20 +1,4 @@
 <?php include("libraries/includes/links.php") ?>
 <?php include("libraries/resources/signupNavbar.php"); ?>
 <?php include("views/forgotpass.views.php"); ?>
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"
-></script>
-
-
-<script src="https://unpkg.com/vue/dist/vue.js"></script>
-<script src="https://unpkg.com/element-ui/lib/index.js"></script>
-<script src="https://unpkg.com/element-ui/lib/umd/locale/en.js"></script>
-
-<script type="module" src="libraries/js/http.js"></script>
-<script type="module" src="libraries/js/main.js"></script>
-<script src="libraries/js/global.js"></script>
-<script type="module" src="libraries/js/vue.js"></script>
+<?php include("libraries/includes/scriptslogin.php") ?>
