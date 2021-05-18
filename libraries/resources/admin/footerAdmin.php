53da0a877a16aa94f173f4912a3ede012e8b6837
heynnahh <ubaldohannah8@gmail.com>
2021-05-19 00:45:52 +0800

    for student dashboard reference only

---
+++ libraries/resources/admin/footerAdmin.php
@@ -0,0 +1,16 @@
+<div class="footerAdmin">
+    <p class="links">
+        <a href="#" >Languages</a>                      
+        <a href="#" >Support</a>
+        <a href="#" >About</a>
+        <a href="#" >Career</a>   
+        <a href="#" >Privacy</a>
+        <a href="#" >Terms of Service</a>
+        <a href="#" >Contact Us</a> 
+        <a href="#" >Facebook</a>
+    </p> 
+    <p class="footer-section-content" id="demo1" >
+        Torres Technology Corporation &copy;
+        <script> document.write(new Date().getFullYear())</script>
+    </p>
+</div>
---
+++ libraries/resources/admin/leftsidebar.php
@@ -0,0 +1,22 @@
+ <div class="card" style="margin-bottom: 10px">
+    <div class="leftside">
+        <div class="row" style="margin-top: 10px;">
+            <h3>Welcome</h3>
+        </div>
+        <div class="container2">
+            <i class="fas fa-user" id="usericons"></i>
+            <div class="text">
+                <h5>Admin Name</h5>
+                <a href="">View Profile</a>
+            </div>
+        </div>
+    </div>
+        <center> <hr width="85%" style="margin-bottom: 50px"> </center>    
+</div>
+<div class="card">
+    <div class="leftside1">
+        <div class="row" style="margin-top: 10px;">
+            <h3>Classes</h3>
+        </div>
+    </div>
+</div>
