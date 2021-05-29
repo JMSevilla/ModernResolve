88e8c436d210b02be318a699a7950b8ff6dc68ef
Benar Isais <benaremail@gmail.com>
2021-05-29 22:46:38 +0800

    adding two stored procedure on migration

--- migration/app/Helpers/Helpers.php
+++ migration/app/Helpers/Helpers.php
@@ -1,5 +1,10 @@
 <?php
 spl_autoload_register('provide_root');
+
+if(isset($_POST['sprocTrigger'])==1){
+    $callback = new SprocModelClass();
+    $callback->sprocModelFunction();
+}
 if(isset($_POST['userTrigger']) == 1){
     // echo json_encode(array('helper' => 'call ok'));
     $column = '(userID int NOT NULL auto_increment, firstname varchar(255), lastname varchar(255) NOT NULL, birth_date datetime, age int, gender varchar(255), contact_number varchar(255), province varchar(255), municipality varchar(255), zip_code varchar(255), address text, email_address varchar(255) unique, password varchar(255), is_verified char, is_type char, is_activate char, is_token_verified char, apikey varchar(6500), created_at datetime default current_timestamp, PRIMARY KEY (userID) )';
